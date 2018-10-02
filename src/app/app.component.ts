c3a8c40a463ce9948e22229aef982ad4aa3dba68
Oleksandr Poshtaruk <aleksandr.poshtaruk@tonicforhealth.com>
2018-10-02 17:28:23 +0300

    Third try

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
-import { Intercom } from 'ng-intercom';
+import {Intercom} from 'ng-intercom';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent implements OnInit {
-  title = 'my-app';
+    title = 'my-app';
 
-  constructor(
-    private intercom: Intercom
-  ) {
-    console.log('config in app.component ', (window as any).CONFIG);
-  }
+    constructor(
+        private intercom: Intercom
+    ) {
+        console.log('config in app.component ', (window as any).CONFIG);
+    }
 
-  ngOnInit() {
-    console.log('intercom ', this.intercom);
-    this.intercom.boot({
-      app_id : (window as any).CONFIG.intercomKey,
-      custom_launcher_selector : '#customIntercomButton'
-    });
-  }
+    ngOnInit() {
+        (this.intercom as any).config.appId = (window as any).CONFIG.intercomKey;
+
+        this.intercom.boot({
+            app_id: (window as any).CONFIG.intercomKey,
+            custom_launcher_selector: '#customIntercomButton'
+        });
+
+
+        console.log('intercom ', this.intercom);
+    }
 }
